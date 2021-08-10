61894dac95d49a0cb0e8be08e5d0e52c92f3a5dd
Messquerade <annac.klingberg@gmail.com>
2021-08-10 11:41:43 -0700

    add highlight lecture paragraphs js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".highlight1").click(function() {
+    $(".highlight1").addClass("highlight");
+  });
+
+  $(".highlight2").click(function() {
+    $(".highlight2").addClass("highlight");
+  });
+});
